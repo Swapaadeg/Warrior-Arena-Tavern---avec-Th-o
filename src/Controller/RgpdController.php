5a365b8e07f96a8db4b611358a986a43cb1503ec
Marie Rivier <marie.rivier23@gmail.com>
2025-08-26 14:58:05 +0200

    ah je suis sur le main? Oups

---
+++ src/Controller/RgpdController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RgpdController extends AbstractController
+{
+    #[Route('/politique-confidentialite', name: 'app_politique_confidentialite')]
+    public function politiqueConfidentialite(): Response
+    {
+        return $this->render('rgpd/politique_confidentialite.html.twig');
+    }
+
+    #[Route('/conditions-generales', name: 'app_conditions_generales')]
+    public function conditionsGenerales(): Response
+    {
+        return $this->render('rgpd/conditions_generales.html.twig');
+    }
+
+    #[Route('/mentions-legales', name: 'app_mentions_legales')]
+    public function mentionsLegales(): Response
+    {
+        return $this->render('rgpd/mentions_legales.html.twig');
+    }
+
+    #[Route('/404', name: 'app_404')]
+    public function page404(): Response
+    {
+        return $this->render('rgpd/404.html.twig', [], new Response('', 404));
+    }
+}
